1bcc3641fe8861808697b979a0029c3aac33e835
yukebrillianth <yukebrilliant@gmail.com>
2024-05-09 17:07:30 +0700

    fix: no login route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Fix no login route
+Route::redirect('/login-redirect', '/login')->name('login');
