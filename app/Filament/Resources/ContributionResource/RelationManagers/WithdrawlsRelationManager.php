f540ab54f9d241723d9659a514c749052d2b91b5
yukebrillianth <yukebrilliant@gmail.com>
2024-05-10 16:36:18 +0700

    Fix: eliminates home options that have been withdrawn

--- app/Filament/Resources/ContributionResource/RelationManagers/WithdrawlsRelationManager.php
+++ app/Filament/Resources/ContributionResource/RelationManagers/WithdrawlsRelationManager.php
@@ -20,18 +20,27 @@ class WithdrawlsRelationManager extends RelationManager
 
     public function form(Form $form): Form
     {
+        $contributionId = $this->getOwnerRecord()->id;
         return $form
             ->schema([
-                Forms\Components\TextInput::make('contribution_id')
-                    ->label('id')
-                    ->default(function (RelationManager $livewire): string {
-                        return $livewire->getOwnerRecord()->pluck('id')[0];
-                    })
-                    ->readonly()
-                    ->required(),
+                // Forms\Components\TextInput::make('contribution_id')
+                //     ->label('id')
+                //     ->default(function (RelationManager $livewire): string {
+                //         return $livewire->getOwnerRecord()->pluck('id')[0];
+                //     })
+                //     ->readonly()
+                //     ->required(),
                 Forms\Components\Select::make('house_id')
-                    ->relationship('house', 'name', function (Builder $query,) {
-                        $query->where('is_active', true)->orderBy('name', 'ASC');
+                    ->relationship('house', 'name', function (Builder $query) use ($contributionId) {
+                        $query->where('is_active', true)
+                            ->whereNotExists(function ($subQuery) use ($contributionId) {
+                                $subQuery->select('house_id')
+                                    ->from('withdrawls')
+                                    ->whereColumn('withdrawls.house_id', 'houses.id')
+                                    ->where('withdrawls.contribution_id', $contributionId)
+                                    ->where('withdrawls.deleted_at', null);
+                            })
+                            ->orderBy('name', 'ASC');
                     })
                     ->getOptionLabelFromRecordUsing(fn (House $record) => "{$record->name} ({$record->holder})")
                     ->label('Rumah')
@@ -103,6 +112,7 @@ class WithdrawlsRelationManager extends RelationManager
                 Tables\Actions\CreateAction::make()
                     ->mutateFormDataUsing(function (array $data): array {
                         $data['user_id'] = auth()->id();
+                        $data['contribution_id'] = $this->getOwnerRecord()->id;
                         if ($data['value']) {
                             $data['is_contribute'] = true;
                         } else {
--- app/Models/House.php
+++ app/Models/House.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class House extends Model
@@ -20,4 +21,9 @@ class House extends Model
         'holder',
         'is_active'
     ];
+
+    public function withdrawls(): HasMany
+    {
+        return $this->hasMany(Withdrawl::class);
+    }
 }
