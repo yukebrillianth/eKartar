c884723e311cc6328511433fd710b8799b29dba7
yukebrillianth <yukebrilliant@gmail.com>
2024-05-11 14:37:59 +0700

    feat: add calculation complete action

--- app/Filament/Resources/ContributionResource/Pages/ViewContribution.php
+++ app/Filament/Resources/ContributionResource/Pages/ViewContribution.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\ContributionResource\Pages;
 
 use App\Filament\Resources\ContributionResource;
+use App\Filament\Resources\ContributionResource\Widgets\ContributionDetailOverview;
+use App\Models\Contribution;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewContribution extends ViewRecord
 {
@@ -13,7 +17,45 @@ class ViewContribution extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            Actions\EditAction::make()
+                ->icon('heroicon-o-pencil-square')
+                ->label('Ubah data'),
+            Action::make('finish')
+                ->label(function (Contribution $record) {
+                    if ($record->is_calculation_complete) {
+                        return 'Penarikan Diselesaikan';
+                    } else {
+                        return 'Selesaikan';
+                    }
+                })
+                ->icon(function (Contribution $record) {
+                    if ($record->is_calculation_complete) {
+                        return 'heroicon-o-check-badge';
+                    }
+                })
+                ->button()
+                ->action(fn (Contribution $record) => $record->completeCalc())
+                ->color('success')
+                ->hidden(function (Contribution $record) {
+                    return !$record->withdrawls->count();
+                })
+                ->disabled(function (Contribution $record) {
+                    return $record->is_calculation_complete;
+                }),
+            Action::make('Umumkan')
+                ->icon('heroicon-o-paper-airplane')
+                ->button()
+                ->color('info')
+                ->hidden(function (Contribution $record) {
+                    return !$record->withdrawls->count() || $record->is_anounced ||  !$record->is_calculation_complete;
+                })
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            ContributionDetailOverview::class,
         ];
     }
 }
