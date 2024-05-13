4913599e38823619531ede57a303520d4120a542
yukebrillianth <yukebrilliant@gmail.com>
2024-05-14 01:00:57 +0700

    deploy

--- deploy.php
+++ deploy.php
@@ -3,6 +3,7 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/php-fpm.php';
 require 'contrib/npm.php';
 
 // Config
@@ -17,11 +18,16 @@ set('keep_releases', 5);
 add('shared_files', ['.env']);
 add('shared_dirs', ['storage']);
 add('writable_dirs', [
-    "bootstrap/cache",
-    "storage",
-    "storage/app",
-    "storage/framework",
-    "storage/logs",
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/cache/data',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
 ]);
 
 set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
@@ -29,9 +35,9 @@ set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction 
 // Hosts
 
 host('production')
+    ->setHostname("nvdc1.duckdns.org")
     ->set('remote_user', 'yukebrillianth')
     ->set('port', 64000)
-    ->setHostname("nvdc1.duckdns.org")
     ->set('branch', 'main')
     ->set('deploy_path', '~/eKartar');
 
@@ -47,31 +53,32 @@ task('deploy:build', [
     'npm:install',
 ]);
 
+task('icon:cache', [
+    '{{bin/php}} artisan icons:cache',
+]);
+
 task('deploy', [
     'deploy:prepare',
     'deploy:secrets',       // Deploy secrets
     'deploy:vendors',
     'deploy:shared',
+    'deploy:build',
     'artisan:storage:link',
-    'artisan:queue:restart',
+    // 'icons:cache',
     'artisan:view:cache',
     'artisan:config:cache',
     'artisan:route:cache',
     'artisan:event:cache',
     'artisan:optimize',
+    'php-fpm:reload',
     'deploy:publish',
     'deploy:unlock',
-]);
-
-task('icons:cache', [
-    run('{{bin/php}} artisan icons:cache'),
 ]);
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release. Uncomment below code if you want to migrate after deploy
-
 before('deploy:symlink', 'artisan:migrate');
 after('deploy:cleanup', 'artisan:cache:clear');
 after('deploy:cleanup', 'artisan:optimize');
