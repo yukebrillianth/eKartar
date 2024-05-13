107e14659715bfd9773e5b3bf7c01c7e1449d7e2
yukebrillianth <yukebrilliant@gmail.com>
2024-05-14 00:25:33 +0700

    deploy

---
+++ deploy.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/npm.php';
+
+// Config
+
+set('bin/php', function () {
+    return '/usr/bin/php'; // change
+});
+set('application', 'eKartar');
+set('repository', 'https://github.com/yukebrillianth/eKartar.git');
+set('keep_releases', 5);
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+add('writable_dirs', [
+    "bootstrap/cache",
+    "storage",
+    "storage/app",
+    "storage/framework",
+    "storage/logs",
+]);
+
+set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
+
+// Hosts
+
+host('production')
+    ->set('remote_user', 'yukebrillianth')
+    ->set('port', 64000)
+    ->setHostname("nvdc1.duckdns.org")
+    ->set('branch', 'main')
+    ->set('deploy_path', '~/eKartar');
+
+// Hooks
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+desc('Build assets');
+task('deploy:build', [
+    'npm:install',
+]);
+
+task('artisan:optimize', [
+    run('{{bin/php}} artisan optimize'),
+]);
+
+task('icons:cache', [
+    run('{{bin/php}} artisan icons:cache'),
+]);
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:secrets',       // Deploy secrets
+    'deploy:vendors',
+    'deploy:shared',
+    'artisan:storage:link',
+    'artisan:queue:restart',
+    'deploy:publish',
+    'deploy:unlock',
+    'icons:cache',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:routes:cache',
+    'artisan:events:cache',
+    'artisan:optimize'
+]);
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release. Uncomment below code if you want to migrate after deploy
+
+before('deploy:symlink', 'artisan:migrate');
