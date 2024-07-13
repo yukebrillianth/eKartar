f3a9eff4e1d77adf23532d5da2570b375ea22870
yukebrillianth <yukebrilliant@gmail.com>
2024-07-13 14:37:40 +0700

    fix: balance update race condition

--- app/Traits/TransactionableTrait.php
+++ app/Traits/TransactionableTrait.php
@@ -5,30 +5,28 @@ namespace App\Traits;
 use App\Enums\TransactionType;
 use App\Models\Balance;
 use App\Models\Transaction;
-use Carbon\Carbon;
 
 trait TransactionableTrait
 {
-    protected function updateBalance(Transaction $transaction, int $value = null): void
+    protected function updateBalance(Transaction $transaction): void
     {
-        $currentBalance = Balance::latest()->lockForUpdate()->first();
+        $currentBalance = Balance::OrderBy('id', 'desc')->latest()->lockForUpdate()->first();
 
         switch ($transaction->type) {
             case TransactionType::Debit:
-                $newBalanceValue = ($currentBalance->value ?? 0) + $value;
+                $newBalanceValue = ($currentBalance->value ?? 0) + $transaction->value;
                 break;
             case TransactionType::Credit:
-                $newBalanceValue = ($currentBalance->value ?? 0) - $value;
+                $newBalanceValue = ($currentBalance->value ?? 0) - $transaction->value;
                 break;
             default:
                 throw new \InvalidArgumentException("Invalid transaction type!");
         }
 
-        $balance = new Balance([
+        Balance::create([
             'value' => $newBalanceValue,
-            'date' => Carbon::now(),
-            'transaction_id' => $transaction->id
+            'date' => now(),
+            'transaction_id' => $transaction->id,
         ]);
-        $balance->save();
     }
 }
